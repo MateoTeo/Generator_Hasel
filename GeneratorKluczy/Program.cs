d0f11a662c4db522102b6d1d2112689acd9d0118
MateoTeo <mateuszbeltowski98@gmail.com>
2022-03-26 17:51:31 +0100

    v 1.0.1

--- GeneratorKluczy/Program.cs
+++ GeneratorKluczy/Program.cs
@@ -15,25 +15,82 @@ namespace GeneratorKluczy
             Random rnd = new Random();
 
 
-            string[] znaki = new string[]{"!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/",
-                          "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?",
-                          "@", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
-                          "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "[", "\\", "]", "^", "_",
-                          "'", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
-                          "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~"};
-                      
+            string[] tablicaZnakowZpecjalnych = new string[] {"!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/","{", "|", "}", "~", ":", ";", "<", "=", ">", "?",  "[", "\\", "]", "^", "_", "'", "@"};
+            string[] tablicaCyfr = new string[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            string[] tablicaDuzychLiter = new string[] {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O","P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            string[] tablicaMalychLiter = new string[] {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o","p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+
+            string[] znaki = new string[] { };
+
+
+            Console.WriteLine("Czy chcesz uzywać znaków specjalnych? (tak/nie) ");
+            string znakiSpecjalne = Console.ReadLine();
+            znakiSpecjalne.ToLower();
+
+            Console.WriteLine("Czy chcesz używać cyfr? (tak/nie)");
+            string cyfry = Console.ReadLine();
+            cyfry.ToLower();
+
+            Console.WriteLine("Czy chcesz używać dużych liter? (tak/nie)");
+            string duzeLitery = Console.ReadLine();
+            duzeLitery.ToLower();
+
+            Console.WriteLine("Czy chcesz używać małych liter? (tak/nie)");
+            string maleLitery = Console.ReadLine();
+            maleLitery.ToLower();
 
             Console.WriteLine("Podaj ilość znaków do stworzenia klucza \nmaksymalna ilość 255");
             ilosc = byte.Parse(Console.ReadLine());
-                        
+
+
+
+
+
+
+
+
+            if (znakiSpecjalne == "tak")
+            {
+                znaki = znaki.Concat(tablicaZnakowZpecjalnych).ToArray();
+            }
+            else
+            {
+
+            }
+            if (cyfry == "tak")
+            {
+                znaki = znaki.Concat(tablicaCyfr).ToArray();
+            }
+            else
+            {
+
+            }
+            if (duzeLitery == "tak")
+            {
+                znaki = znaki.Concat(tablicaDuzychLiter).ToArray();
+            }
+            else
+            {
+
+            }
+            if (maleLitery == "tak")
+            {
+                znaki = znaki.Concat(tablicaMalychLiter).ToArray();
+            }
+            else
+            {
+                    
+            }
+
+
             for (int i = 0; i <= ilosc; i++)
-            {                
-                int pozycja = rnd.Next(0, 93);
+            {
+                int pozycja = rnd.Next(0, znaki.Length);
                 klucz += znaki[pozycja];
             }
             Console.WriteLine(klucz);
 
-            Console.ReadKey();            
+            Console.ReadKey();
         }
 
         
